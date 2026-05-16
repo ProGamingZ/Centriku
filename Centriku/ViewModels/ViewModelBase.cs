ebd948c17160d1b261c4f9103545b67f2aec4df5
YJAO <ortizyuri254@gmail.com>
2026-05-16 11:42:47 +0800

    Centriku Initialized

---
+++ Centriku/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Centriku.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ Centriku/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Centriku.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
